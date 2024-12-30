2da25d5241ac6c0ddefb23d216b5694756e97826
Timmydevsoft <johnaytech@gmail.com>
2024-12-30 15:21:13 +0100

    added other origin

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ dbconnect()
 dotenv.config()
 const app = express()
 const  coreOptions= {
-    origin: "https://ems-7200.onrender.com",
+    origin: ["https://ems-7200.onrender.com", "http://localhost:5173"],
     credentials: true,
     methods:['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders:[
